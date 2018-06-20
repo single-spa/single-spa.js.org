4a4bf8fc4ccbfce17786dda5f52d7bb3318d832a
alocke12992 <alocke12992@gmail.com>
2018-06-20 13:54:50 -0600

    Adds backtoTop button

--- .circleci/pushConfig.sh
+++
@@ -1,6 +0,0 @@
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=776dc995f391ff95851eafe1c4853bad256da6b9 \
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/CanopyTax/single-spa.js.org/tree/master
