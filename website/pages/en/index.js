6621829064322244508cd4032e5d932da4b08423
alocke12992 <alocke12992@gmail.com>
2018-06-20 16:32:43 -0600

    Minor revision to getting started

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -83,7 +83,7 @@ class HomeSplash extends React.Component {
           <ProjectTitle />
           <SubHeading />
           <PromoSection>
-            <Button href={docUrl("building-applications.html")}>Get Started</Button>
+            <Button href={docUrl("starting-from-scratch.html")}>Get Started</Button>
             <Button href="https://single-spa.surge.sh" target="_blank">See a live example</Button>
           </PromoSection>
         </div>
