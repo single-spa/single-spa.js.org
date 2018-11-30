dce18995e14255bab1c830e40cae778376c85fe4
Carlos Filoteo <carlos.filoteo@podium.com>
2018-11-30 00:43:01 -0700

    Simplify contributors page and retrieval

--- website/pages/en/contributors.js
+++ website/pages/en/contributors.js
@@ -3,49 +3,58 @@ const CompLibrary = require('../../core/CompLibrary.js');
 const Container = CompLibrary.Container;
 
 const projects = [
-  {title: "single-spa", divId: "contributors"},
-  {title: "single-spa-angularJS", divId: "angularJS"},
-  {title: "single-spa-angular", divId: "angular"},
-
+  { user: "CanopyTax", repo: "single-spa" },
+  { user: "CanopyTax", repo: "single-spa-angularJS" },
+  { user: "CanopyTax", repo: "single-spa-angular" },
+  { user: "PlaceMe-SAS", repo: "single-spa-angular-cli" },
+  { user: "pcmnac", repo: "single-spa-cycle" },
+  { user: "CanopyTax", repo: "single-spa-ember" },
+  { user: "CanopyTax", repo: "single-spa-inferno" },
+  { user: "CanopyTax", repo: "single-spa-preact" },
+  { user: "CanopyTax", repo: "single-spa-react" },
+  { user: "CanopyTax", repo: "single-spa-svelte" },
+  { user: "CanopyTax", repo: "single-spa-vue" }
 ]
 
-class Contributors extends React.Component{
-  showCont(){
-    const projects = [
-      { user: "CanopyTax", repo: "single-spa", divId: "contributors" },
-      { user: "CanopyTax", repo: "single-spa-angularJS", divId: "angularJS" },
-      { user: "CanopyTax", repo: "single-spa-angular", divId: "angular" },
-      { user: "PlaceMe-SAS", repo: "single-spa-angular-cli", divId: "angularCli" },
-      { user: "pcmnac", repo: "single-spa-cycle", divId: "cycle" },
-      { user: "CanopyTax", repo: "single-spa-ember", divId: "ember" },
-      { user: "CanopyTax", repo: "single-spa-inferno", divId: "inferno" },
-      { user: "CanopyTax", repo: "single-spa-preact", divId: "preact" },
-      { user: "CanopyTax", repo: "single-spa-react", divId: "react" },
-      { user: "CanopyTax", repo: "single-spa-svelte", divId: "svelte" },
-      { user: "CanopyTax", repo: "single-spa-vue", divId: "vue" },
+const getContributors = () => (
+`document.addEventListener("DOMContentLoaded", () => {
+  ${JSON.stringify(projects)}.forEach(({user, repo}) => {
+    fetch(\`https://api.github.com/repos/\${user}/\${repo}/contributors\`)
+      .then(res => res.json())
+      .then(data => {
+        const contributerElems = data.map(({html_url, login, id, avatar_url}) => (\`  
+          <a href="\${html_url}" target="_blank" ref="noopener noreferrer" class="contributor" id=\${id}>
+            <h3 class="contributorLogin">
+              \${login}
+            </h3>
+            <img class="contributorImg" src=\${avatar_url} alt="\${login}"s Github avatar" />
+          </a>  
+        \`)).join("")
+        document.getElementById(\`\${repo}-contributors\`).innerHTML = contributerElems;
+      })
+      .catch(error => console.error("Error:", error))
+  })
+})`)
 
-    ]
-    return(
-      projects.map( (project, i) => (
-        <div className="contributorWrapper" key={i}>
-          <a 
-            href={`https://github.com/${project.user}/${project.repo}`} 
-            className="repoLink" 
-            target="_blank"
-            rel="noopener noreferrer"
-          >{project.repo}</a>
-          <div id={project.divId}></div>
-        </div>
-      ))
-    )
-  }
+class Contributors extends React.Component{
   render(){
     return(
       <div className="mainContainer">
         <Container padding={['bottom']}>
           <h1 className="contributorHeader">Contributors</h1>
-          {this.showCont()}
+          {projects.map(({user, repo, contributors}) => (
+            <div className="contributorWrapper" key={repo}>
+              <a 
+                href={`https://github.com/${user}/${repo}`} 
+                className="repoLink" 
+                target="_blank"
+                rel="noopener noreferrer"
+              >{repo}</a>
+              <div id={`${repo}-contributors`}></div>
+            </div>
+          ))}
         </Container>
+        <script dangerouslySetInnerHTML={{__html: getContributors()}}></script>
       </div>
     )
   }
