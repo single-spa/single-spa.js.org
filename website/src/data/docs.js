36a7925faa935deb74e73dc75a6fe2fd681f2ce2
Alexey Pyltsyn <lex61rus@gmail.com>
2019-11-09 08:31:39 +0300

    Fix links (#124)

--- website/src/data/docs.js
+++ website/src/data/docs.js
@@ -4,14 +4,14 @@ const items = [
     content: `Opinionated and easy interactive tutorial. SystemJS without the pain. <br><br> [Go to Playground](https://single-spa-playground.org)`,
     imageUrl: 'img/icons/148705-essential-collection/svg/map-1.svg',
     imageAlt: 'map with path',
-    imageLink: 'docs/getting-started-overview/',
+    imageLink: '/docs/getting-started-overview/',
   },
   {
     title: `Examples and starter kits`,
     content: `Github repos ready to clone. For all major frameworks and build systems.<br><br> [Go to Examples](/docs/examples/)`,
     imageUrl: 'img/icons/148705-essential-collection/svg/blueprint.svg',
     imageAlt: 'house blueprint',
-    imageLink: 'docs/examples/',
+    imageLink: '/docs/examples/',
   },
   {
     title: 'Tutorials',
@@ -19,14 +19,14 @@ const items = [
       'Written tutorials with step-by-step instructions for different frameworks.<br><br> [Go to Tutorials](/docs/starting-from-scratch/)',
     imageUrl: 'img/icons/148705-essential-collection/svg/list.svg',
     imageAlt: 'checklist',
-    imageLink: 'docs/getting-started-overview/',
+    imageLink: '/docs/getting-started-overview/',
   },
   {
     title: 'API Documentaion',
     content: `Documentation of concepts, exported functions, and interop with frameworks.<br><br> [Go to API Documentation](/docs/getting-started-overview/)`,
     imageUrl: 'img/icons/148705-essential-collection/svg/more-2.svg',
     imageAlt: 'code documentation',
-    imageLink: 'docs/getting-started-overview/',
+    imageLink: '/docs/getting-started-overview/',
   },
 ];
 
