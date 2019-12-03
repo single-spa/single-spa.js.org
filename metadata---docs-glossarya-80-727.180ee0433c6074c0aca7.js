9e7a9922749795cf1d0fb16a15a4c3bcf3345fc0
CircleCI on behalf of Anthony Frehner <frehner@users.noreply.github.com>
2019-12-03 13:28:46 +0000

    Deploy website version based on 455960ac548e1ffa1d36c1f51a8a38b518c5006f

---
+++ metadata---docs-glossarya-80-727.180ee0433c6074c0aca7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[75],{195:function(s){s.exports=JSON.parse('{"id":"glossary","title":"Glossary","sidebar_label":"Glossary","description":"<dl>","source":"@site/../docs/glossary.md","permalink":"/docs/glossary","editUrl":"https://github.com/CanopyTax/single-spa.js.org/blob/master/docs/glossary.md"}')}}]);
