4957cdc4957f4e17eb8d4baa45c9eb35c1369075
CircleCI on behalf of Anthony Frehner <frehner@users.noreply.github.com>
2019-12-03 19:17:03 +0000

    Deploy website version based on c9e807394587c27a9e60ab81ba20c10b19f5fa2e

---
+++ metadata---docs-faq-431-8a6.15237ddc852111670933.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[73],{198:function(s){s.exports=JSON.parse('{"id":"faq","title":"Frequently Asked Questions","sidebar_label":"FAQ","description":"## What does single-spa do?","source":"@site/../docs/faq.md","permalink":"/docs/faq","editUrl":"https://github.com/CanopyTax/single-spa.js.org/blob/master/docs/faq.md"}')}}]);
