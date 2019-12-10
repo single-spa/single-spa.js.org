dee8d820afc0a06b2137dab313e109ef8b721913
Joel Denning <joeldenning@gmail.com>
2019-12-10 10:12:08 +0200

    Trying to fix error page

--- website/src/plugins/docusaurus-plugin-redirects/src/index.js
+++ website/src/plugins/docusaurus-plugin-redirects/src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 
 const DEFAULT_OPTIONS = {
-  excludedPaths: [],
+  excludedPaths: ['/error'],
 };
 
 module.exports = function(context, opts) {
