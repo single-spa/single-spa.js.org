88613bca2b101c79e73b61d12b190ddae7bb837c
Joel Denning <joeldenning@gmail.com>
2020-01-14 09:58:31 -0700

    Adding six more error code documentation pages.

---
+++ website/src/components/Errors/Codes/21.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import CodeSnippet from '../CodeSnippet'
+
+export default function ErrorCode21(props) {
+  return (
+    <>
+      <h1>#21: Duplicate application name</h1>
+      <p>
+        Cannot register application '{props.getErrorCodeArg(0)}' because an application has already been registered with that name.
+      </p>
+      <h2>To fix:</h2>
+      <div>
+        <CodeSnippet>
+          {`
+            import { registerApplication } from 'single-spa';
+
+            registerApplication(
+              'some-other-name',
+              loadingFunction,
+              activityFunction
+            )
+          `}
+        </CodeSnippet>
+      </div>
+      <h2>Explanation:</h2>
+      <p>
+        See <a href="/docs/api#registerApplication">registerApplication documentation</a>.
+      </p>
+    </>
+  )
+}
