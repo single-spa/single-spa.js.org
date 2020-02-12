7a37c5ff31b9bb1fad374087ee1befc91161b1be
Joel Denning <joeldenning@gmail.com>
2020-02-11 22:22:16 -0700

    Updating examples, tutorials, and sidebar

--- website/src/pages/docs/index.js
+++ website/src/pages/docs/index.js
@@ -1,39 +1,9 @@
 import React from 'react';
-import Markdown from 'markdown-to-jsx';
-import classnames from 'classnames';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import Layout from '@theme/Layout';
-import items from '@site/src/data/docs';
-import styles from './styles.module.css';
+import { Redirect } from '@docusaurus/router'
 
 function Docs() {
   return (
-    <Layout>
-      <div
-        className="container container--fluid padding-horiz--xl margin-top--xl"
-        style={{ marginBottom: '6.8rem' }} // simulate sticky footer
-      >
-        <div className="row">
-          {items.map(
-            ({ title, content, imageUrl, imageAlt, imageLink }, idx) => (
-              <div
-                key={idx}
-                className={classnames('col col--3 text--center', styles.doc)}>
-                <div className={styles.docImage}>
-                  <a href={imageLink}>
-                    <img src={useBaseUrl(imageUrl)} alt={imageAlt} />
-                  </a>
-                </div>
-                <h2 className="margin-top--lg">{title}</h2>
-                <p>
-                  <Markdown>{content}</Markdown>
-                </p>
-              </div>
-            ),
-          )}
-        </div>
-      </div>
-    </Layout>
+    <Redirect to="/docs/getting-started-overview" />
   );
 }
 
