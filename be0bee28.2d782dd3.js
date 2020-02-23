8acc27f04c491222e85de75193b38ff849526a73
CircleCI on behalf of Anthony Frehner <frehner@users.noreply.github.com>
2020-02-23 18:53:34 +0000

    Deploy website version based on a0faf0c71d1f1bd2036d2d901c0b0d7a6e080601

--- be0bee28.2d782dd3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[108],{236:function(s){s.exports=JSON.parse('{"id":"glossary","title":"Glossary","description":"<dl>","source":"@site/../docs/glossary.md","permalink":"/docs/next/glossary","editUrl":"https://github.com/single-spa/single-spa.js.org/blob/master/website/../docs/glossary.md","version":"next","sidebar_label":"Glossary"}')}}]);
