8e78edfc645f9ccc6a271c95fcc2404e5bf268ba
Justin McMurdie <mcmurdiej@gmail.com>
2020-05-14 09:29:53 -0600

    Adding a callout to canopy (#256)

--- website/src/theme/Footer/index.js
+++ website/src/theme/Footer/index.js
@@ -88,17 +88,18 @@ function Footer() {
             ))}
           </div>
         )}
-        {(logo || copyright) && (
-          <div className="text--center">
-            {logo && logo.src && (
-              <img
-                className="footer__logo margin-bottom--sm"
-                alt={logo.alt} src={useBaseUrl(logo.src)}
-              />
-            )}
-            {copyright}
+        <div className="text--center">
+          {logo && logo.src && (
+            <img
+              className="footer__logo margin-bottom--sm"
+              alt={logo.alt} src={useBaseUrl(logo.src)}
+            />
+          )}
+          <div>
+            Originally developed at <a href='https://getcanopy.com'>Canopy</a>
           </div>
-        )}
+          {copyright && copyright}
+        </div>
       </div>
     </footer>
   );
