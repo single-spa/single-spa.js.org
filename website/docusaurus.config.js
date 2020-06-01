e5ce2fe5176f09005da857cf047f8a89a5d32689
Bret Little <blittle@salesforce.com>
2020-06-01 17:19:42 -0400

    Add accessibility changes (#265)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   scripts: [
     'https://unpkg.com/vanilla-back-to-top@7.2.1/dist/vanilla-back-to-top.min.js',
-    'https://cdn.jsdelivr.net/npm/racial-equity-banner@1.0.0/racial-equity-banner-bottom.js',
+    'https://cdn.jsdelivr.net/npm/racial-equity-banner@1.0.1/racial-equity-banner-bottom.js',
     '/js/index.js',
   ],
   themeConfig: {
