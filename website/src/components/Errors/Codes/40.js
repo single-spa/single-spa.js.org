ad8c110f0231e2da758a763112803325de1a9696
Joel Denning <joeldenning@gmail.com>
2020-06-09 12:15:05 -0600

    Adding error code page for error 40. (#278)

---
+++ website/src/components/Errors/Codes/40.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function ErrorCode40(props) {
+  return (
+    <>
+      <h1>#40: Invalid custom props returned from function</h1>
+      <p>
+        The single-spa application {props.getErrorCodeArg(0)} was registered with a function for its customProps.
+        That function must return an object of custom props, but instead returned {props.getErrorCodeArg(1)}.
+      </p>
+      <p>
+        See the <a href="/docs/configuration#custom-props">custom props documentation</a> for more information.
+      </p>
+    </>
+  );
+}
