dc7846d3f3147d6e674dd36161f19fcdd00aa66f
Joel Denning <joeldenning@gmail.com>
2020-11-20 10:46:21 -0700

    Set url in docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const repoUrl = 'https://github.com/single-spa/single-spa.js.org';
+const repoUrl = 'https://github.com/single-spa/ru.single-spa.js.org';
 const libRepoUrl = 'https://github.com/single-spa/single-spa';
 
 const versions = require('./versions.json');
@@ -7,7 +7,7 @@ const versions = require('./versions.json');
 module.exports = {
   title: 'single-spa',
   tagline: 'A javascript router for front-end microservices',
-  url: 'https://single-spa.github.io/single-spa.js.org',
+  url: 'https://single-spa.github.io/ru.single-spa.js.org',
   baseUrl: '/',
   projectName: 'single-spa',
   favicon: 'img/logo-blue-favicon.ico',
