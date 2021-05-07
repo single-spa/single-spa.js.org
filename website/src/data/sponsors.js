b4c8fc5cd3f4a1896ae52de871c74819e3309ad0
Joel Denning <joeldenning@gmail.com>
2021-05-07 17:29:13 -0600

    Add data camp

--- website/src/data/sponsors.js
+++ website/src/data/sponsors.js
@@ -1,5 +1,11 @@
 const sponsors = [
   {
+    caption: 'Data Camp',
+    image: 'img/data_camp_logo.png',
+    infoLink: 'https://www.datacamp.com/',
+    pinned: true
+  },
+  {
     caption: 'Toast',
     image: 'img/Toast-Logo.png',
     infoLink: 'https://pos.toasttab.com/',
