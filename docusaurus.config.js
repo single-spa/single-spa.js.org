1fe91f9df3bae0dfadf0fb4ce680e9f2ac88b1ae
Joel Denning <joeldenning@gmail.com>
2024-09-09 15:05:42 -0600

    Add governance page (#641)
    Co-authored-by: Joel Denning <joeldenning@Joels-Laptop.local>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,6 +74,10 @@ module.exports = {
           title: "Community",
           items: [
             {
+              label: "Governance",
+              to: "governance/",
+            },
+            {
               label: "Code of Conduct",
               to: "code-of-conduct/"
             },
