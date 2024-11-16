211cc9b3f60de79387085eeae57120043f78ed01
Joel Denning <joeldenning@gmail.com>
2024-11-15 17:01:50 -0700

    Update sponsors list (#657)

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -1,20 +1,8 @@
 const sponsors = [
   {
-    caption: "Data Camp",
-    image: "img/data_camp_logo.png",
-    infoLink: "https://www.datacamp.com/",
-    pinned: true,
-  },
-  {
-    caption: "Toast",
-    image: "img/Toast-Logo.png",
-    infoLink: "https://pos.toasttab.com/",
-    pinned: true,
-  },
-  {
-    caption: "Asurion",
-    image: "img/asurion.svg",
-    infoLink: "https://medium.com/asurion-product-development",
+    caption: "Joel Denning",
+    image: "img/franklin.jpg",
+    infoLink: "https://www.single-spa-workshop.com/",
     pinned: true,
   },
 ];
